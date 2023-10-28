78ce298d5db4e48561b50b40b0cc61368ad62cd0
Marichka0701 <tanechnyk.marichka07@gmail.com>
2023-10-28 19:07:52 +0300

    test

---
+++ src/routing/router.js
@@ -0,0 +1,24 @@
+import {createBrowserRouter, Navigate} from "react-router-dom";
+
+import {MAIN_ROUTES} from "./main_routes";
+import MainLayout from "../layouts/MainLayout/MainLayout";
+import LoginPage from "../pages/LoginPage/LoginPage";
+
+const user = localStorage.getItem("token");
+
+export const router = createBrowserRouter([
+    {
+        index: true,
+        element: <Navigate to={MAIN_ROUTES.LIVE_TRACKING}/>
+        // element: user ? <Navigate to={MAIN_ROUTES.LIVE_TRACKING}/> : <Navigate to={MAIN_ROUTES.LOGIN}/>
+    },
+    {
+        path: MAIN_ROUTES.LIVE_TRACKING,
+        // element: user ? <MainLayout /> : <Navigate to={MAIN_ROUTES.LOGIN} />,
+        element: <MainLayout/>,
+    },
+    {
+        path: MAIN_ROUTES.LOGIN,
+        element: <LoginPage/>,
+    }
+]);
